366a77a3461658fab6c3575ac3fe1bb679718a51
Benjamin Howes <91109173+capnganj@users.noreply.github.com>
2022-06-05 17:28:18 -0400

    lots of trial and error
    saving a place or two along the way...  This is a sort-of working spot where stereogram reads the threejs canvas and draws in 3d ... but depthmapper is giving what looks like two-tone results; not a ton of depth.
    Also trying to get an image-based depthmapper to work as downloading /screenshotting and using the online image-based demo gives good results

--- src/index.js
+++ src/index.js
@@ -44,60 +44,90 @@ let previewed = false;
 
 
 //global vars 
-var controls, renderer, scene, camera, threeCanvas, magicCanvas;
+var controls, renderer, scene, camera, threeCanvas, magicCanvas, imageElement;
 init();
 
 function init() {
   //scene & camera
   scene = new THREE.Scene();
   //let bc = feet.desaturateColor(feet.color.background, 1.5);
-  //scene.background = new THREE.Color(bc.r/255, bc.g/255, bc.b/255);
+  //scene.background = new THREE.Color(0,0,0);
 
   renderer = new THREE.WebGLRenderer( { 
     antialias: true,
-    alpha: true
+    alpha: true,
+    preserveDrawingBuffer: true
   } );
-  
   renderer.setPixelRatio( window.devicePixelRatio );
   renderer.setSize( window.innerWidth, window.innerHeight );
   renderer.domElement.id = "hashish";
   //document.body.appendChild( renderer.domElement );
   threeCanvas = renderer.domElement;
 
+  //image for the depthmapper to chew on - three generates this... but did not work last night
+  // imageElement = document.createElement('img');
+  // imageElement.id = "depthImage";
+  // imageElement.width = window.innerWidth;
+  // imageElement.height = window.innerHeight;
+  //document.body.appendChild(imageElement);
+  
+
   //create a canvas for the magicmagic
-  magicCanvas = document.createElement("CANVAS");
+  magicCanvas = document.createElement('canvas');
   magicCanvas.id = "magicHashish";
   magicCanvas.width = window.innerWidth;
   magicCanvas.height = window.innerHeight;
   document.body.appendChild(magicCanvas);
 
-  camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1.5, 4.5 );
-  camera.position.set( 0, 0.5 , 4 );
-  //controls = new OrbitControls( camera, renderer.domElement );
-
-
-
-  //geometry
-  const b = new THREE.IcosahedronGeometry(1.5);
-
-  const t = new THREE.TorusBufferGeometry(1, 0.5, 20, 50)
-  t.rotateX(feet.map(fxrand(), 0, 1, 0, Math.PI * 2))
-  t.rotateY(feet.map(fxrand(), 0, 1, 0, Math.PI * 2))
-  t.rotateZ(feet.map(fxrand(), 0, 1, 0, Math.PI * 2))
-
-  let f = new MushroomCap(1.5, 1).fungualBufferGeometry;
-  
-
-
+  camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1.2, 6 );
+  camera.position.set( 0, 0.5, 3.1 );
+  camera.updateProjectionMatrix();
+  controls = new OrbitControls( camera, renderer.domElement );
+  controls.target.set(0,0.5,0)
 
 
   //material
-  const m = new THREE.MeshDepthMaterial( );
+  const m = new THREE.MeshDepthMaterial();
   const n = new THREE.MeshNormalMaterial();
 
-  const mesh = new THREE.Mesh(f, m);
-  mesh.position.set(1, 1, 0)
-  scene.add(mesh);
+
+  //geometry
+
+  //ellipse gives evenly spaced points 
+  const elle = new THREE.EllipseCurve(0,0, 2.2, 1.1, 0, Math.PI*2, false, 0);
+  const numShrooms = 7;
+  const basePointsOnXY = elle.getPoints(numShrooms)
+
+  for (let i = 0; i < numShrooms; i++) {
+    
+    //cap
+    const capWidth = feet.map(fxrand(), 0, 1, 0.3, 0.75);
+    const f = new MushroomCap(capWidth, feet.map(fxrand(), 0, 1, 0.2, 0.5)).fungualBufferGeometry;
+    const mesh = new THREE.Mesh(f, m);
+    
+    const centerPoint = new THREE.Vector3(basePointsOnXY[i].x, feet.map(fxrand(), 0, 1, 0.1, 1.5), basePointsOnXY[i].y)
+    mesh.position.set(centerPoint.x, centerPoint.y, centerPoint.z)
+    scene.add(mesh);
+
+    //stem
+    const insetFactor = feet.map(fxrand(), 0, 1, 0.25, 0.85);
+    const stemCrv = new THREE.CubicBezierCurve3(
+      new THREE.Vector3(centerPoint.x, centerPoint.y, centerPoint.z),
+      new THREE.Vector3(centerPoint.x, centerPoint.y * 0.65, centerPoint.z),
+      new THREE.Vector3(centerPoint.x * insetFactor, centerPoint.y * 0.15, centerPoint.z * insetFactor),
+      new THREE.Vector3(centerPoint.x * insetFactor, -1.0, centerPoint.z * insetFactor)
+    );
+    const tube = new THREE.TubeBufferGeometry(stemCrv, 100, capWidth * 0.125, 50, false);
+    const stemMesh = new THREE.Mesh(tube, m);
+    scene.add(stemMesh);
+  }
+
+  const boxer = new THREE.BoxBufferGeometry(6,4,3.2);
+  //flip normals
+  boxer.applyMatrix4(new THREE.Matrix4().makeScale(-1,1,1));
+  const boxMesh = new THREE.Mesh(boxer, m);
+  boxMesh.position.set(0,1,0)
+  //scene.add(boxMesh);
 
   const axis = new THREE.AxesHelper(1);
   //scene.add(axis);
@@ -110,8 +140,22 @@ function init() {
   //set up resize listener and let it rip!
   window.addEventListener( 'resize', onWindowResize );
   //animate();
+
+  //render the first frame
   render();
+
+  //populate the image source with the data url 
+  // imageElement.src = document.getElementById('hashish').toDataURL();
+  // imageElement.onload = () => {
+  //   magicDust();
+  //   document.body.removeChild(imageElement);
+  // }
+  //document.body.removeChild( renderer.domElement );
+  // document.body.removeChild(imageElement);
+
+  //generate the stereogram
   magicDust();
+  //download();
 }
 
 
@@ -153,7 +197,7 @@ function render() {
 
 function download() {
   var link = document.createElement('a');
-  link.download = 'AcidDrops.png';
+  link.download = 'MagicMushrooms.png';
   link.href = document.getElementById('hashish').toDataURL()
   link.click();
 }
@@ -163,5 +207,6 @@ function magicDust() {
     el: 'magicHashish',
     colors: [feet.interpolateFn(0.05), feet.interpolateFn(0.5), feet.interpolateFn(0.95)],
     depthMapper: new Stereogram.CanvasDepthMapper(threeCanvas)
+    //depthMapper: new Stereogram.ImgDepthMapper(imageElement)
   });
 }
